63cd4c2d71c1dd47269605bed7058f9f4e4853ae
tynskid <67418614+tynskid@users.noreply.github.com>
2023-07-14 02:29:31 -0400

    Update globalStyles.ts

--- frontend/lib/src/theme/globalStyles.ts
+++ frontend/lib/src/theme/globalStyles.ts
@@ -1157,4 +1157,34 @@ div[data-testid="stSidebarNav"] li div a:hover {
   .footer {
   display:none;
 }
+                                                #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-uf99v8.e1g8pov65 > div.block-container.css-z5fcl4.e1g8pov64 > div:nth-child(1) > div > div.css-ocqkz7.esravye3 > div:nth-child(1) > div:nth-child(1) > div > div:nth-child(1) > div > div:nth-child(2) > div > p {
+                                                margin-bottom:30px;
+                                                }
+
+                                                #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-uf99v8.e1g8pov65 > div.block-container.css-z5fcl4.e1g8pov64 > div:nth-child(1) > div > div.css-ocqkz7.esravye3 > div:nth-child(1) > div:nth-child(1) > div > div:nth-child(1) > div > div:nth-child(2) > div > div:nth-child(1){
+                                                  //display: flex;
+                                                  //align-items: center;
+                                                
+                                                }
+
+
+
+
+
+                                       #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-uf99v8.ea3mdgi5 > div.block-container.css-z5fcl4.ea3mdgi4 > div:nth-child(1) > div > div.css-ocqkz7.e1f1d6gn3 > div:nth-child(1) > div:nth-child(1) > div > div:nth-child(1) > div > div:nth-child(2) > div  button
+                                       #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-uf99v8.e1g8pov65 > div.block-container.css-z5fcl4.e1g8pov64 > div:nth-child(1) > div > div.css-ocqkz7.esravye3 > div:nth-child(1) > div:nth-child(1) > div > div:nth-child(1) > div > div:nth-child(2) > div button {
+  background-color: transparent !important;
+  padding-top:2px;
+  padding-bottom:0px;
+  padding-left:0px;
+  padding-right:0px;
+  margin:0px;
+}
+
+
+                                       #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-uf99v8.e1g8pov65 > div.block-container.css-z5fcl4.e1g8pov64 > div:nth-child(1) > div > div.css-ocqkz7.esravye3 > div:nth-child(1) > div:nth-child(1) > div > div:nth-child(1) > div > div:nth-child(2) > div button p{
+                                       color:#000000 !important;
+                                       } 
+
+
 `
